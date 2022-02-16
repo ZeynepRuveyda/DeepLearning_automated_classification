2fc0aaed64718da047aa385ccf2726522728350c
ZeynepRuveyda <ozelz3805@gmail.com>
2022-02-16 02:01:53 +0100

    some changing

--- model.py
+++ model.py
@@ -5,27 +5,26 @@ import tensorflow as tf
 import inspect
 
 
-class Custom_Model(tf.keras.Model):
-    def _init_(self,  model_name, image_size, weights='imagenet'):
-        super(Custom_Model, self).__init__()
-        self.input_shape = image_size
-        self.weights = weights
+class ModelSubClassing(tf.keras.Model):
+    def __init__(self,model_name,IMG_SHAPE):
+        super(ModelSubClassing, self).__init__()
+        # define all layers in init
         self.model_name = model_name
-        self.model_dictionary = {m[0]: m[1] for m in inspect.getmembers(tf.keras.applications, inspect.isfunction)}
-        self.model_dictionary.pop('NASNetLarge')
-        self.base_model = self.model_dictionary[self.model_name](input_shape=self.input_shape,
-                                                                 include_top=False,weights = self.weights)
-        self.base_model.trainable = False
+        self.IMG_SHAPE = IMG_SHAPE
         self.dense = tf.keras.layers.Dense(1)
 
-    def call(self, inputs,training=False):
-        print(self.base_model.summary())
-        x = self.base_model(inputs,training=training)
+    def call(self, input_tensor, training=False):
+        model_dictionary = {m[0]: m[1] for m in inspect.getmembers(tf.keras.applications, inspect.isfunction)}
+        model_dictionary.pop('NASNetLarge')
+        base_model = model_dictionary[self.model_name](input_shape=self.input_shape,include_top=False,weights = self.weights)
+        base_model.trainable = False
+        x = base_model(input_tensor,training=training)
         x = tf.keras.layers.GlobalAveragePooling2D(x)
         x = tf.keras.layers.Dropout(0.2)(x)
         outputs = self.dense(x)
         return outputs
 
+
     def build_graph(self):
-        x = tf.keras.layers.Input(shape=self.input_shape)
+        x = tf.keras.layers.Input(shape=self.IMG_SHAPE)
         return tf.keras.Model(inputs=[x], outputs=self.call(x))
