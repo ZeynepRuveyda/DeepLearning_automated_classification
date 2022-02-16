d472b468fb41a696f3b0b8134667042829ac734a
ZeynepRuveyda <ozelz3805@gmail.com>
2022-02-16 02:30:36 +0100

    some added feature

--- model.py
+++ model.py
@@ -14,16 +14,20 @@ class ModelSubClassing(tf.keras.Model):
         self.dense = tf.keras.layers.Dense(1)
 
     def call(self, input_tensor):
+
         model_dictionary = {m[0]: m[1] for m in inspect.getmembers(tf.keras.applications, inspect.isfunction)}
         model_dictionary.pop('NASNetLarge')
+        pre_model = model_dictionary[self.model_name].preprocess_input
         base_model = model_dictionary[self.model_name](input_shape=self.input_shape,include_top=False,weights = self.weights)
         base_model.trainable = False
-        x = base_model(input_tensor,training=False)
+        x = pre_model(input_tensor)
+        x = base_model(x,training=False)
         x = tf.keras.layers.GlobalAveragePooling2D(x)
         x = tf.keras.layers.Dropout(0.2)(x)
         outputs = self.dense(x)
         return outputs
 
-    def build_graph(self):
+    def summary(self):
         x = tf.keras.layers.Input(shape=self.IMG_SHAPE)
-        return tf.keras.Model(inputs=[x], outputs=self.call(x))
+        model = tf.keras.Model(inputs=[x], outputs=self.call(x))
+        return model.summary()
