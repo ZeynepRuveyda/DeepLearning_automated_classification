30e4654205cfdbe4367d8aeb5975ee4099ee6255
ZeynepRuveyda <ozelz3805@gmail.com>
2022-02-20 15:34:44 +0100

    without keras Model

--- model.py
+++ model.py
@@ -5,27 +5,26 @@ import tensorflow as tf
 import inspect
 
 
-class Custom_Model(tf.keras.Model):
-    def __init__(self, model_name,IMG_SHAPE):
-        super(Custom_Model, self).__init__()
+class Custom_Model():
+    def __init__(self, model_name,im_size):
+
         # define all layers in init
-        self.IMG_SHAPE = IMG_SHAPE
+        self.IMG_SHAPE = im_size + (3,)
         self.model_name = model_name
         self.dense = tf.keras.layers.Dense(1, activation=tf.nn.softmax)
         self.flat = tf.keras.layers.Flatten(name="flatten")
         self.model_dictionary = {m[0]: m[1] for m in inspect.getmembers(tf.keras.applications, inspect.isfunction)}
-        self.model_dictionary.pop('NASNetLarge')
         self.base_model = self.model_dictionary[self.model_name](input_shape=self.IMG_SHAPE, include_top=False,
                                                                  weights='imagenet')
         self.base_model.trainable = False
         self.global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
         self.dropout = tf.keras.layers.Dropout(0.2)
 
-    def call(self,i):
-        input_ = tf.keras.Input(shape=self.IMG_SHAPE)
-        x = self.base_model(input_,training=False)
+    def forward(self):
+        input = tf.keras.Input(shape=self.IMG_SHAPE)
+        x = self.base_model(input,training=False)
         x = self.global_average_layer(x)
         x = self.dropout(x)
         outputs = self.dense(x)
-        model = tf.keras.Model(input_,outputs)
+        model = tf.keras.Model(input,outputs)
         return model
