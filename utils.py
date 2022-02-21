7ce74489763cf5fc9cd11fee068ca46ac9688043
ZeynepRuveyda <ozelz3805@gmail.com>
2022-02-21 01:29:33 +0100

    class_weigths_added

--- utils.py
+++ utils.py
@@ -120,9 +120,8 @@ def random_over_sampling(train_df,all_image_path):
                 im = cv2.imread(os.path.join(os.path.expanduser('~'), all_image_path, sample['id']), -1)
                 cv2.imwrite(os.path.join(os.path.expanduser('~'), all_image_path, 'copy_' + sample['id']), im)
 
-    new_names = [x + y for x, y in zip(list(train_df.id), names)]
-    print(new_names)
-    new_labels = [x + y for x, y in zip(list(train_df.label), labels)]
+    new_names = list(train_df.id) + names
+    new_labels = list(train_df.label) + labels
     new_train_df = pd.DataFrame({'id': new_names, 'label': new_labels})
 
 
