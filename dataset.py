a68a638a452c1a479eb978994e68dc9f8455e553
ZeynepRuveyda <ozelz3805@gmail.com>
2022-02-21 23:42:58 +0100

    random over sampling made in colab

--- dataset.py
+++ dataset.py
@@ -18,7 +18,6 @@ class CustomDataGenerator:
         self.train_df = train_df
         self.batch_size = batch_size
         self.target_size = target_size
-        self.nm_classes = len(self.train_df.label.unique().tolist())
 
     def data_generator(self):
         with tf.device('/device:GPU:0'):
@@ -60,49 +59,19 @@ class CustomDataGenerator:
                 seed=42,
                 class_mode="categorical"
             )
-        counts = train_df.label.value_counts()
-        count_dict = counts.to_dict()
-        class_weights = create_class_weight(count_dict, self.nm_classes)
-
-        if balance_check(self.train_df):
-
-            with tf.device('/device:GPU:0'):
-                print("Training Dataset : ")
-                train_generator = train_datagen.flow_from_dataframe(
-                    dataframe=train_df,
-                    directory=self.images_folder_dir,
-                    x_col="id",
-                    y_col="label",
-                    target_size=self.target_size,
-                    batch_size=self.batch_size,
-                    shuffle=True,
-                    seed=42,
-                    class_mode="categorical"
-                )
-                return train_generator, val_generator, test_generator,self.nm_classes
-        else:
-
-
-            print("Class distribution : %s ,Balancing process is started." % (count_dict))
-            train_df_new = random_over_sampling(train_df, self.images_folder_dir)
-            with tf.device('/device:GPU:0'):
-                print("Training Dataset : ")
-                train_generator = train_datagen.flow_from_dataframe(
-                    dataframe=train_df_new,
-                    directory=self.images_folder_dir,
-                    x_col="id",
-                    y_col="label",
-                    target_size=(self.target_size, self.target_size),
-                    batch_size=self.batch_size,
-                    shuffle=True,
-                    seed=42,
-                    class_mode="categorical"
-                )
-                counts = train_df_new.label.value_counts()
-                count_dict = counts.to_dict()
-
-                print("Class distribution : %s ,Data made balance." % (count_dict))
-                return train_generator, val_generator, test_generator, self.nm_classes, class_weights
+            print("Training Dataset : ")
+            train_generator = train_datagen.flow_from_dataframe(
+                dataframe=train_df,
+                directory=self.images_folder_dir,
+                x_col="id",
+                y_col="label",
+                target_size=self.target_size,
+                batch_size=self.batch_size,
+                shuffle=True,
+                seed=42,
+                class_mode="categorical"
+            )
+            return train_generator, val_generator, test_generator
 
 
 
