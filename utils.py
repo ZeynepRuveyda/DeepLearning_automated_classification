d3695fac6d2d2f59e4d6d9c3a23fb8189c0a3eb5
ZeynepRuveyda <ozelz3805@gmail.com>
2022-02-21 02:37:42 +0100

    model_selection

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import cv2
 from sklearn.model_selection import train_test_split
 import time
 import random
+import seaborn as sns
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,6 +20,7 @@ from model import *
 # Getting all images into a one folder
 # Return two data csv and all images data path.
 from tqdm import tqdm
+from sklearn.metrics import confusion_matrix
 def create_dataset(dir_zip):
     image_types = ['.png', '.jpg', '.jpeg', '.tiff', '.bmp']
     with ZipFile(dir_zip, 'r') as zipObj:
@@ -153,7 +155,7 @@ def model_selection(train_generator,validation_generator,im_size,nm_classes):
 
         model_ = Custom_Model(model_name,im_size,nm_classes)
         # custom modifications on top of pre-trained model
-        model_ = model.forward()
+        model_ = model_.forward()
         base_learning_rate = 0.0001
         model_.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
                       loss='binary_crossentropy',
@@ -210,3 +212,20 @@ def metrics_visualization(history):
     plt.xlabel('epoch')
 
     return plt.show()
+
+
+def plot_cm(labels, predictions, p=0.5):
+  cm = confusion_matrix(labels, predictions > p)
+  plt.figure(figsize=(5,5))
+  sns.heatmap(cm, annot=True, fmt="d")
+  plt.title('Confusion matrix @{:.2f}'.format(p))
+  plt.ylabel('Actual label')
+  plt.xlabel('Predicted label')
+
+  print('Legitimate Transactions Detected (True Negatives): ', cm[0][0])
+  print('Legitimate Transactions Incorrectly Detected (False Positives): ', cm[0][1])
+  print('Fraudulent Transactions Missed (False Negatives): ', cm[1][0])
+  print('Fraudulent Transactions Detected (True Positives): ', cm[1][1])
+  print('Total Fraudulent Transactions: ', np.sum(cm[1]))
+
+  return plt.show
